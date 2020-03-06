4f95565a65a9d188a69b503aaa493ae720cae4ed
JeeYounL <35190771+JeeYounL@users.noreply.github.com>
2020-03-06 23:58:44 +0900

    Create vanillaindex.js

---
+++ vanillaindex.js
@@ -0,0 +1,18 @@
+const Jeeyoun={
+   name:"Jeeyoun",
+   age:23,
+   gender: "female",
+   isPretty: true,
+   favSeries:["Brooklyn99","Lucifer"],
+   favFood:[
+      {
+         name:"ramyun",
+         fat: true
+      },
+      {
+         name:"lettuce",
+         fat:false
+      }
+   ]
+}
+console.log(Jeeyoun.favSeries[1]);
